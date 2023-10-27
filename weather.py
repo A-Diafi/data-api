54aa4f8ffe3bf7f3fae20770b3f61d9356d54020
Amin Diafi <amindiafi.ad@gmail.com>
2023-10-27 07:32:22 +0100

    File that passes tests

--- weather.py
+++ weather.py
@@ -12,19 +12,43 @@ def search_city(query):
     Look for a given city. If multiple options are returned, have the user choose between them.
     Return one city (or None)
     '''
-    pass  # YOUR CODE HERE
+    url = "https://weather.lewagon.com/geo/1.0/direct?q=" + query + "&limit=5"
+    response = requests.get(url).json()
+    if len(response) > 1:
+        for i, city in enumerate(response, 1):
+            print(f'{i}: {city["name"]}, {city["country"]}')
+        idx = int(input("Multiple matches found, which city did you mean?: ")) - 1
+        return response[idx]
+    elif len(response) == 1:
+        return response[0]
+    else:
+        return None
 
 def weather_forecast(lat, lon):
-    '''Return a 5-day weather forecast for the city, given its latitude and longitude.'''
-    pass  # YOUR CODE HERE
+
+    url = f"https://weather.lewagon.com/data/2.5/forecast?lat={lat}&lon={lon}"
+    response = requests.get(url).json()
+    new = response["list"]
+    centigrade_symbol = '\u00b0'
+    list1 = []
+    dict = {}
+    for i in range(33):
+        if i % 8 == 0:
+            list1.append({'weather':f'{new[i]["dt_txt"].split(" ")[0]} {new[i]["weather"][0]["description"]} ({round(float(new[i]["main"]["temp_max"]) - 273.15)}{centigrade_symbol}C)'})
+    return list1
 
 def main():
     '''Ask user for a city and display weather forecast'''
     query = input("City?\n> ")
     city = search_city(query)
+    #need this to return a dict therefore we need to run the length check inside
+    forecast = weather_forecast(city['lat'], city['lon'])
+    print(forecast)
+    for i in forecast:
+        for key, value in i.items():
+                print(f"{key} {value}")
+    return city
 
-    # TODO: Display weather forecast for a given city
-    pass  # YOUR CODE HERE
 
 if __name__ == '__main__':
     try:
